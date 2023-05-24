474c659489d68eee424f046da1a5ddf84f9aebfc
Adholah96 <adholah96@gmail.com>
2023-05-24 19:16:44 +0300

    Display all countires

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,7 @@ import './App.css';
 import Nav from './components/nav';
 import { Filter } from './components/filter';
 import { Country } from './components/country';
-
-const countries = {
-  name: 'Afghanistan',
-  capital: 'Kabul',
-  region: 'Asia',
-  population: 40218234,
-  flags: {
-    svg: 'https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_the_Taliban.svg',
-    png: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Flag_of_the_Taliban.svg/320px-Flag_of_the_Taliban.svg.png',
-  },
-};
+import data from '/data.json';
 
 function App() {
   return (
@@ -20,20 +10,16 @@ function App() {
       <Nav />
       <Filter />
       <div className="country-holder">
-        <Country
-          title={countries.name}
-          capital={countries.capital}
-          region={countries.region}
-          population={countries.population}
-          image={countries.flags.png}
-        />
-        <Country
-          title={countries.name}
-          capital={countries.capital}
-          region={countries.region}
-          population={countries.population}
-          image={countries.flags.png}
-        />
+        {data.map((countries, i) => (
+          <Country
+            key={i}
+            title={countries.name}
+            capital={countries.capital}
+            region={countries.region}
+            population={countries.population}
+            image={countries.flags.png}
+          />
+        ))}
       </div>
     </>
   );
